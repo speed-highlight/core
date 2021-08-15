831528fcd07e5bed03f897df89598c2cfaa88b47
Matubu <78111467+Matubu@users.noreply.github.com>
2021-08-15 23:52:49 +0200

    regex lang supported + perfomance

--- syntax.js
+++ syntax.js
@@ -1,14 +1,55 @@
-const shg_lang = {
-	//regex: [
-	//],
+const shg_lang = {//str \stuff
+	markdown: [
+		{
+			match: /(#{1,3} .*)/g,
+			type: 'var'
+		}
+	],
+	regex: [
+		{
+			match: /(\w*$)/g,
+			type: 'key'
+		},
+		{
+			match: /(\[((?!\])[^\\]||\\.)*\])/g,
+			type: 'num'
+		},
+		{
+			match: /(\^|\$)/g,
+			type: 'key'
+		},
+		{
+			match: /(\\.|\*|\+|\|\{\d+,\d+\})/g,
+			type: 'var'
+		}
+	],
+	jsdoc: [
+		{
+			match: /(@\w+)/g,
+			type: 'key'
+		},
+		{
+			match: /({[\w\s|<>,.@\[\]]+})/g,
+			type: 'num'
+		},
+		{
+			match: /(\[[\w\s="']+\])/g,
+			type: 'var'
+		}
+	],
 	js: [
 		{
 			match: /(^#!\/.*)/g,
-			type: 'comment',
+			type: 'cmt'
+		},
+		{
+			match: /(\/\*\*((?!\*\/)[^])*(\*\/)?)/g,
+			type: 'cmt',
+			lang: 'jsdoc'
 		},
 		{
 			match: /(\/\/.*\n?|\/\*((?!\*\/)[^])*(\*\/)?)/g,
-			type: 'comment'
+			type: 'cmt'
 		},
 		{
 			match: /((["'])((?!\2)[^\r\n\\]||\\.)*\2?)/g,
@@ -19,63 +60,65 @@ const shg_lang = {
 			type: 'bool'
 		},
 		{
-			match: /(?<=[\s{}();,])(=>|as|async|await|break|case|catch|class|const|constructor|continue|debugger|default|delete|do|else|enum|export|extends|finally|for|from|function|get|if|implements|import|in|instanceof|interface|let|var|set|of|new|package|private|protected|public|return|static|super|switch|throw|throws|try|typeof|void|while|with|yield)(?=[\s{}();,])/g,
-			type: 'keyword'
+			match: /\b(as|async|await|break|case|catch|class|const|constructor|continue|debugger|default|delete|do|else|enum|export|extends|finally|for|from|function|get|if|implements|import|in|instanceof|interface|let|var|set|of|new|package|private|protected|public|return|static|super|switch|throw|throws|try|typeof|void|while|with|yield)\b/g,
+			//match: /(?<=[\s{}();,])(=>|as|async|await|break|case|catch|class|const|constructor|continue|debugger|default|delete|do|else|enum|export|extends|finally|for|from|function|get|if|implements|import|in|instanceof|interface|let|var|set|of|new|package|private|protected|public|return|static|super|switch|throw|throws|try|typeof|void|while|with|yield)(?=[\s{}();,])/g,
+			type: 'key'
 		},
 		{
-			match: /\b(([0-9])+(e[0-9]+)?(\.[0-9]+)?|NaN|null|undefined)\b/g,
+			match: /\b(\d+(e\d+)?(\.\d+)?|NaN|null|undefined)\b/g,
 			type: 'num'
 		},
 		{
-			match: /([/*+:?&|%^~=!,<>.-]+)/g,
-			type: 'operator'
+			match: /(\/((?!\/)[^\r\n\\]||\\.)+\/[dgimsuy]*)/g,
+			type: 'func',
+			lang: 'regex'
 		},
 		{
-			match: /([\w$][\w0-9$]*)(?=\s*(\?\.)?\s*\()/g,
+			match: /([/*+:?&|%^~=!,<>.-]+)/g,
+			type: 'oper'
+		},
+		{
+			match: /([\w$][\w\d$]*)(?=\s*(\?\.)?\s*\()/g,
 			type: 'func'
 		},
 		{
-			match: /((?<=\.\s*)\w+|\bthis)\b/g,
+			match: /(\bthis\b|\w+\s*:)/g,
+			//match: /((?<=\.\s*)\w+\b|\bthis\b|\w+\s*:)/g,
 			type: 'var'
 		},
 		{
-			match: /(?<=class\s+)(\w+)/g,
+			match: /\b(class\s+)(\w+)/g,
 			type: 'class'
 		}
-		//highlight: 'python'
 		//template literall
-		//regex
-		//jsdoc
 	]
 }
 
 const shg = (_ => {
-	const sanitize = str => str.replaceAll('<', '&lt;').replaceAll('>', '&gt;'),
-		highlightElement = elm => {
-			let src = elm.innerText,
-				res = '',
+	const sanitize = str => str?.replaceAll?.('<', '&lt;')?.replaceAll?.('>', '&gt;'),
+		highlightText = (src, lang) => {
+			let res = '',
 				index,
 				match,
 				firstPart,
 				firstIndex,
 				firstMatch,
-				i = 0,
-				lang = shg_lang.js;
+				i = 0;
 
-			const add = (str, type) => res += type ? `<span class='syn-${type}'>${sanitize(str)}</span>` : sanitize(str);
+			const addUnsafe = (str, type) => res += type ? `<span class='syn-${type}'>${str}</span>` : str,
+				add = (str, type) => addUnsafe(sanitize(str), type);
 
+			lang.forEach(part => part.last = undefined);
 			while (i < src.length) {
 				firstIndex = null;
-				lang = lang.filter(part => {
+				lang = lang.filter(part => {//filter not most memory efficient
 					if (firstIndex === 0)
 						return 1;
 					if (part.last == undefined || part.last.index < i) {
 						part.match.lastIndex = i;
 						match = part.match.exec(src);
-						if (match === null) {
-							part.last = undefined;
+						if (match === null)
 							return 0;
-						}
 						part.last = match;
 					}
 					index = part.last.index;
@@ -85,21 +128,21 @@ const shg = (_ => {
 						firstIndex = index;
 						firstMatch = match;
 					}
-					else
-						part.last = undefined;
 					return 1;
 				});
 				if (firstIndex === null)
 					break;
 				add(src.slice(i, firstIndex));
-				add(firstMatch, firstPart.type);
+				if (firstPart.lang)
+					addUnsafe(highlightText(firstMatch, shg_lang[firstPart.lang]), firstPart.type);
+				else
+					add(firstMatch, firstPart.type);
 				i = firstPart.match.lastIndex;
 			}
 			add(src.slice(i, src.length));
-		 	elm.innerHTML = res;
+			return res;
 		}
+		highlightElement = elm => elm.innerHTML = highlightText(elm.innerText, shg_lang.js);
 
-	return {
-		highlightElement
-	}
+	return { highlightElement }
 })()
