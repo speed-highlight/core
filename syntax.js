b45c865811d4c13f85b84bd27cbd9437d2eff402
Matubu <78111467+Matubu@users.noreply.github.com>
2021-08-15 15:32:06 +0200

    fix for coment matcher + string matcher + perfomance
    fix a case where the regex matcher was not working : 'test' + 'test', it will consider everything as string

--- syntax.js
+++ syntax.js
@@ -1,45 +1,14 @@
-// Prism.languages.clike = {
-// 	'comment': [
-// 		{
-// 			pattern: /(^|[^\\])\/\*[\s\S]*?(?:\*\/|$)/,
-// 			lookbehind: true,
-// 			greedy: true
-// 		},
-// 		{
-// 			pattern: /(^|[^\\:])\/\/.*/,
-// 			lookbehind: true,
-// 			greedy: true
-// 		}
-// 	],
-// 	'string': {
-// 		pattern: /(["'])(?:\\(?:\r\n|[\s\S])|(?!\1)[^\\\r\n])*\1/,
-// 		greedy: true
-// 	},
-// 	'class-name': {
-// 		pattern: /(\b(?:class|interface|extends|implements|trait|instanceof|new)\s+|\bcatch\s+\()[\w.\\]+/i,
-// 		lookbehind: true,
-// 		inside: {
-// 			'punctuation': /[.\\]/
-// 		}
-// 	},
-// 	'keyword': /\b(?:if|else|while|do|for|return|in|instanceof|function|new|try|throw|catch|finally|null|break|continue)\b/,
-// 	'boolean': /\b(?:true|false)\b/,
-// 	'function': /\b\w+(?=\()/,
-// 	'number': /\b0x[\da-f]+\b|(?:\b\d+(?:\.\d*)?|\B\.\d+)(?:e[+-]?\d+)?/i,
-// 	'operator': /[<>]=?|[!=]=?=?|--?|\+\+?|&&?|\|\|?|[?*/~^%]/,
-// 	'punctuation': /[{}[\];(),.:]/
-// };
-
 const shg_lang = {
 	//regex: [
 	//],
 	js: [
 		{
-			match: /(\/\/.*\n?|\/\*[^]*\*\/)/g,
+			//match: /(^#!\/.*|\/\/.*\n?|\/\*[^]*\*\/)/g,
+			match: /(^#!\/.*|\/\/.*\n?|\/\*((?!\*\/)[^])*(\*\/)?)/g,
 			type: 'comment'
 		},
 		{
-			match: /((["']).*[^\\\n]\2)/g,
+			match: /((["'])((?!\2)[^\r\n\\]||\\.)*\2?)/g,
 			type: 'str'
 		},
 		{
@@ -88,112 +57,44 @@ const shg = (_ => {
 				firstIndex,
 				firstMatch,
 				i = 0,
-				part;
+				lang = shg_lang.js;
+
 			const add = (str, type) => res += type ? `<span class='syn-${type}'>${sanitize(str)}</span>` : sanitize(str);
 
 			while (i < src.length) {
-				j = -1;
 				firstIndex = null;
-				for (part of shg_lang.js) {
-					part.match.lastIndex = i;
-					match = part.match.exec(src);
-					if (match !== null) {
-						index = match.index;
-						match = match[1];
-						if (match !== undefined && (index < firstIndex || firstIndex === null))
-						{
-							firstPart = part;
-							firstIndex = index;
-							firstMatch = match;
+				lang = lang.filter(part => {
+					if (firstIndex === 0)
+						return 1;
+					if (part.last == undefined || part.last.index < i) {
+						part.match.lastIndex = i;
+						match = part.match.exec(src);
+						if (match === null) {
+							part.last = undefined;
+							return 0;
 						}
-						if (index === 0)
-							break;
+						part.last = match;
 					}
-				}
+					index = part.last.index;
+					match = part.last[1];
+					if (index < firstIndex || firstIndex === null) {
+						firstPart = part;
+						firstIndex = index;
+						firstMatch = match;
+					}
+					else
+						part.last = undefined;
+					return 1;
+				});
 				if (firstIndex === null)
 					break;
 				add(src.slice(i, firstIndex));
 				add(firstMatch, firstPart.type);
 				i = firstPart.match.lastIndex;
 			}
-			//add last part
 			add(src.slice(i, src.length));
 		 	elm.innerHTML = res;
-			//add end
-
 		}
-	// function highlightElement(elm) {
-	// 	let src = elm.innerText,
-	// 		res = '',
-	// 		i = -1,
-	// 		slice,
-	// 		word,
-	// 		match;
-
-	// 	const add = (str, type) => res += type ? `<span class='syn-${type}'>${sanitize(str)}</span>` : sanitize(str),
-	// 		addWord = (word, type) => add(src.slice(i, (i += word.length - 1) + 1), type);
-	// 		matchWord = str => str.match(/^[a-zA-Z][a-zA-Z0-9_-]*/)?.[0] ?? '',
-	// 		js = exitCondition => {
-	// 			for (; ++i < src.length;) {
-	// 				if (src[i] == exitCondition)
-	// 					break;
-	// 				slice = src.slice(i);
-	// 				word = matchWord(slice);
-	// 				if (match = slice.match(/^(\/\/.*\n?|\/\*[^]*\*\/)/)?.[0])
-	// 					addWord(match, 'comment');
-	// 				else if (['break', 'case', 'catch', 'continue', 'debugger', 'default', 'delete', 'do', 'else', 'export', 'extends', 'finally', 'for', 'function', 'if', 'import', 'in', 'instanceof', 'new', 'return', 'super', 'switch', 'throw', 'try', 'typeof', 'void', 'while', 'with', 'yield', 'enum', 'implements', 'interface', 'package', 'private', 'protected', 'public', 'static', 'yield', 'await', 'async', 'abstract', 'boolean', 'byte', 'char', 'double', 'final', 'float', 'goto', 'int', 'long', 'native', 'short', 'synchronized', 'throws', 'transient', 'volatile', 'constructor']
-	// 					.includes(word))
-	// 					addWord(word, 'keyword');
-	// 				else if (word == 'this')
-	// 					addWord(word, 'var');
-	// 				else if (['let', 'const', 'var', 'class'].includes(word)) {
-	// 					add(src.slice(i, (i += word.length)), 'keyword');
-	// 					add(src.slice(i, (i += src.slice(i).match(/^[ 	\n\r]*/).length)));
-	// 					addWord(matchWord(src.slice(i)), 'var');
-	// 				}
-	// 				else if (['true', 'false'].includes(word))
-	// 					add(src.slice(i, (i += word.length - 1) + 1), 'bool');
-	// 				else if (slice.startsWith('=>'))
-	// 					add(slice.slice(0, 2), 'keyword') && i++;
-	// 				else if (src[i] == '.')
-	// 				{
-	// 					add(src[i++], 'operator');
-	// 					word = matchWord(src.slice(i));
-	// 					add(src.slice(i, (i += word.length - 1) + 1), /^[	 ]*(\?\.)?\(/.test(src.slice(i + 1)) ? 'func' : 'var');
-	// 				}
-	// 				else if (['/', '*', '-', '+', ';', ':', '?', '&', '|', '%', '^', '~', '(', ')', '{', '}', '=', '!', ',', '<', '>'].includes(src[i]))
-	// 					add(src[i], 'operator')
-	// 				else if (match = slice.match(/^[0-9]+e?[0-9]*\.?[0-9]*/)?.[0])
-	// 				{
-	// 					add(src.slice(i, (i += match.length - 1) + 1), 'num');
-	// 				}
-	// 				else if (['NaN', 'null', 'undefined'].includes(word))
-	// 					add(src.slice(i, (i += word.length - 1) + 1), 'num');
-	// 				else if (word)
-	// 					add(src.slice(i, (i += word.length - 1) + 1), /^[	 ]*(\?\.)?\(/.test(src.slice(i + 1)) ? 'func' : null);
-	// 				else if (match = slice.match(/^("[^\n"]*|'[^\n']*)/)?.[0])
-	// 					add(src.slice(i, (i += match.length) + 1), 'str');
-	// 				else if (src[i] == '`') {
-	// 					let start = i++;
-	// 					while (i < src.length && src[i] != '`') {
-	// 						if (src.slice(i, i + 2) == '${') {
-	// 							add(src.slice(start, i), 'str');
-	// 							add(src.slice(i, ++i + 1), 'var');
-	// 							js('}');
-	// 							add(src[i], 'var')
-	// 							start = i + 1;
-	// 						}
-	// 						i++;
-	// 					}
-	// 					add(src.slice(start, i + 1), 'str');
-	// 				}
-	// 				else
-	// 					add(src[i]);
-	// 			}
-	// 		}
-	// 	js();
-	// 	elm.innerHTML = res;
-	// }
 
 	return {
 		highlightElement
