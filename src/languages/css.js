92760470652112a43c6ebe2ce23f874032bde088
matubu <matubu@github>
2022-01-10 14:00:24 +0100

    update css

--- src/languages/css.js
+++ src/languages/css.js
@@ -15,19 +15,42 @@ export default [
 		match: /\b[\w-]+(?=\s*:)|(::?|\.)[\w-]+(?=[^{}]*{)/g
 	},
 	{
-		type: 'oper',
-		match: /[+~>*|,;:=]+/g
-	},
-	{
 		type: 'func',
 		match: /#[\w-]+(?=[^{}]*{)/g
 	},
 	{
 		type: 'num',
-		match: /#[\da-f]{3,8}|\d(\.\d)?(cm|mm|in|px|pt|pc|em|ex|ch|rem|vm|vh|vmin|vmax|%)?/g
+		match:  /#[\da-f]{3,8}/g
+	},
+	{
+		type: 'num',
+		match: /\d+(\.\d+)?(cm|mm|in|px|pt|pc|em|ex|ch|rem|vm|vh|vmin|vmax|%)?/g,
+		sub: [
+			{
+				type: 'var',
+				match: /[a-z]+|%/g
+			}
+		]
+	},
+	{
+		match: /url\([^)]*\)/g,
+		sub: [
+			{
+				type: 'func',
+				match: /url(?=\()/g
+			},
+			{
+				type: 'str',
+				match: /[^()]+/g
+			}
+		]
 	},
 	{
 		type: 'func',
-		match: /[a-zA-Z]\w*(?=\s*\()/g
+		match: /\b[a-zA-Z]\w*(?=\s*\()/g
+	},
+	{
+		type: 'num',
+		match: /\b[a-z-]+\b/g
 	}
 ]
