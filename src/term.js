5b8a983192acafd0cb2db6807dfd09d7da501979
matubu <matubu@github>
2022-03-05 21:40:39 +0100

    fix #24, (Cannot access 'theme' before initialization)

--- src/term.js
+++ src/term.js
@@ -4,16 +4,16 @@
 
 import { tokenize } from './index.js';
 
-let theme = import('./themes/default.js').default;
+let theme = import('./themes/default.js');
 
 export const highlightText = async (src, lang) => {
-	let res = '', theme = await theme;
+	let res = '', themeMap = (await theme).default;
 
-	await tokenize(src, lang, (str, token) => res += token ? `${theme[token] ?? ''}${str}\x1b[0m` : str);
+	await tokenize(src, lang, (str, token) => res += token ? `${themeMap[token] ?? ''}${str}\x1b[0m` : str);
 
 	return res;
 };
 
 export const printHighlight = async (...arg) => console.log(await highlightText(...arg));
 
-export const setTheme = async name => theme = (await import(`./themes/${name}.js`)).default;
+export const setTheme = async name => theme = import(`./themes/${name}.js`);
