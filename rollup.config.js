4bda04bbc8e4d56866c6990411663e218f793ab4
BearToCode <davidebasso03@gmail.com>
2023-07-30 16:03:31 +0200

    add types declarations

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,17 @@
 import dynamicImportVars from '@rollup/plugin-dynamic-import-vars';
 import { terser } from "rollup-plugin-terser";
+import copy from 'rollup-plugin-copy'
+
 
 export default {
 	plugins: [
 		dynamicImportVars(), // Fix dynamics imports
-		terser() // Minify bundle
+		terser(), // Minify bundle
+		copy({ // Copy d.ts files
+			targets: [
+				{ src: 'src/*.d.ts', dest: 'dist' }
+			]
+		})
 	],
 	input: [
 		'src/index.js'
