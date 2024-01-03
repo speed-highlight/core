8519954cd94f8ba134ff9a973d58c8bb80e24502
Matubu <hi@mathias.ninja>
2024-01-04 00:55:54 +0100

    fix #39 and improve assembly highlighting

--- examples/languages/test.asm
+++ examples/languages/test.asm
@@ -11,7 +11,13 @@ section .text
 	mov rsi, msg ; ABI is rdi, rsi
 	mov rdi, fmt
 	mov rax, 0   ; rax is # of non-int args
-	
+
 	call printf
+
+	# https://github.com/speed-highlight/core/issues/39
+	vmovddup	2536(%rbx,%rax), %xmm1          # comment
+	vmovaps	2528(%rbx,%rax), %xmm3
+	vfmadd213ps	%xmm6, %xmm5, %xmm3
+	vpslld	$23, %xmm4, %xmm4
 
 	ret
--- src/languages/asm.js
+++ src/languages/asm.js
@@ -1,7 +1,7 @@
 export default [
 	{
 		type: 'cmnt',
-		match: /;.*/gm
+		match: /(;|#).*/gm
 	},
 	{
 		expand: 'str'
@@ -10,18 +10,26 @@ export default [
 		expand: 'num'
 	},
 	{
+		// value (ex: "$0x1")
+		type: 'num',
+		match: /\$[\da-fA-F]*\b/g
+	},
+	{
 		type: 'kwd',
-		match: /^[a-z]+\s+[a-z.]+/gm,
+		// ex: "section .data"
+		match: /^[a-z]+\s+[a-z.]+\b/gm,
 		sub: [
 			{
+				// keyword (ex: "section")
 				type: 'func',
 				match: /^[a-z]+/g
 			}
 		]
 	},
 	{
+		// instruction (ex: "mov")
 		type: 'kwd',
-		match: /^\t*[a-z]+/gm,
+		match: /^\t*[a-z][a-z\d]*\b/gm,
 	},
 	{
 		match: /%|\$/g,
