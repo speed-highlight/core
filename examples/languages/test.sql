57ca3afa2d331ee88ae666ca01244a825041cbb8
Louis Volland <louis@lvolland.fr>
2026-08-01 05:09:26 +0200

    fix: sql keywords in lowercase
    kwdS? in the keyword list was a typo for KEYS?

--- examples/languages/test.sql
+++ examples/languages/test.sql
@@ -2,5 +2,7 @@
 SELECT 'Geeks' || ' ' || @test_hello FROM dual;
 INSERT INTO database (geek_id, geek_name) VALUES (5000, 'abc');
 REPLACE('123geeks123', '123', TRUE);
+CREATE TABLE geeks (geek_id INT PRIMARY KEY, geek_name VARCHAR(20));
+select count(geek_id) from geeks where geek_name is not null and active = true;
 /* FIXME Multiline
 	Comment */
